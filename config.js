f4efb8255412eeea60a785a2536819bee95c455d
zhurs <zhurs@si-tech.com.cn>
2020-07-30 08:59:29 +0800

    本地配置

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   version: "7.1.2",
   note: '修复商品详情页规格尺寸浮窗没有遮住分享按钮',
   subDomain: "tz", // https://admin.it120.cc 登录后台首页的专属域名
-  appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
+  appid: "wx0953ec0f670e42da", // 您的小程序的appid，购物单功能需要使用
   shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
   requireBindMobile: true, // 是否强制绑定手机号码才能使用
   kanjiaRequirePlayAd: true // 是否必须要观看视频广告后才可以砍价
